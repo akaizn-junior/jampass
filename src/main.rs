1f8480ad3c01fe3c2d31f469e2406c3f654b6502
Simao Nziaka <sdnziaka@gmail.com>
2022-11-29 15:44:43 +0100

    rename

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod core_t;
 mod env;
 mod util;
 
-fn massage_errors(e: core_t::Error) {
+fn handle_error(e: core_t::Error) {
     println!("- {:?}", e);
 }
 
@@ -14,7 +14,7 @@ fn main() {
     let result = cli::parse();
 
     match result {
-        Err(e) => massage_errors(e),
+        Err(e) => handle_error(e),
         _ => {}
     }
 }
